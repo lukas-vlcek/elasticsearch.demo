19bb8aa7a1f88a98452305eb9d90d03bc2372e92
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-06-11 16:36:05 +0200

    Renaming

--- src/test/java/org/elasticsearch/demo/IndexAndSearchTest.java
+++ src/test/java/org/elasticsearch/demo/IndexAndSearchTest.java
@@ -23,7 +23,7 @@ import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilde
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class BasicTest {
+public class IndexAndSearchTest {
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
@@ -31,7 +31,7 @@ public class BasicTest {
 
     @Before
     public void prepare() throws IOException {
-        String tempFolderName = testFolder.newFolder(BasicTest.class.getName()).getCanonicalPath();
+        String tempFolderName = testFolder.newFolder(IndexAndSearchTest.class.getName()).getCanonicalPath();
         settings = settingsBuilder()
                 .put("index.store.type", "memory")
                 .put("gateway.type", "none")
@@ -39,17 +39,13 @@ public class BasicTest {
                 .build();
     }
 
-    private byte[] createSource(String room, String color) throws IOException {
-        return XContentFactory.jsonBuilder()
-                .startObject()
-                .field("room", room)
-                .field("color", color)
-                .endObject()
-                .copiedBytes();
-    }
-
-    @Test
-    public void simple() throws IOException {
+    /**
+     * Index and search data. To make sure we can search for data that has been indexed just now call the refresh.
+     * By default the refresh is executed every second.
+     *
+     * @throws IOException
+     */
+    @Test public void shouldIndexAndSearchRightAfterRefresh() throws IOException {
 
         // For unit tests it is recommended to use local node.
         // This is to ensure that your node will never join existing cluster on the network.
@@ -58,16 +54,20 @@ public class BasicTest {
                 .local(true)
                 .node();
 
+        // get Client
         Client client = node.client();
 
+        // Index some data
         client.prepareIndex("house", "room", "1").setSource(createSource("livingroom", "red")).execute().actionGet();
         client.prepareIndex("house", "room", "2").setSource(createSource("familyroom", "white")).execute().actionGet();
         client.prepareIndex("house", "room", "3").setSource(createSource("kitchen", "blue")).execute().actionGet();
         client.prepareIndex("house", "room", "4").setSource(createSource("bathroom", "white")).execute().actionGet();
         client.prepareIndex("house", "room", "5").setSource(createSource("garage", "blue")).execute().actionGet();
 
+        // Refresh index reader
         client.admin().indices().refresh(Requests.refreshRequest("_all")).actionGet();
 
+        // Prepare and execute query
         QueryBuilder queryBuilder = QueryBuilders.termQuery("color", "white");
 
         SearchResponse resp = client.prepareSearch("house")
@@ -78,6 +78,7 @@ public class BasicTest {
                 .execute()
                 .actionGet();
 
+        // Make sure we got back expected data
         List<String> expected = new ArrayList<String>();
         expected.add("2");
         expected.add("4");
@@ -88,8 +89,18 @@ public class BasicTest {
             assertTrue(expected.contains(hit.id()));
         }
 
+        // cleanup
         client.close();
         node.close();
 
+    }
+
+    private byte[] createSource(String room, String color) throws IOException {
+        return XContentFactory.jsonBuilder()
+                .startObject()
+                .field("room", room)
+                .field("color", color)
+                .endObject()
+                .copiedBytes();
     }
 }
